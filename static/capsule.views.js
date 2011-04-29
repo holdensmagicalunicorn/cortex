2c5f8c41a5bf6ee86184442940ee136cc8731ded
sdiehl <freyrs@gmail.com>
2011-04-29 13:47:06 -0400

    added note about changes to capsule

--- static/capsule.views.js
+++ static/capsule.views.js
@@ -20,6 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+/* Note: Modified from original source */
+
 (function(){
   // Module Setup
   // ------------
